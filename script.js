1a4c4173f303aa010b08bb5da72a7089ef6c87e7
Akorsikov <akors@mail.ru>
2022-01-14 18:29:00 +0300

    feat: create script.js with selfscore

---
+++ script.js
@@ -0,0 +1,37 @@
+function selfscore() {
+    console.log('10/10  Вёрстка валидная +10');
+    console.log('20/20  Вёрстка семантическая +20');
+    console.log('         -в коде  присутствуют <header>, <main>, <footer> +2');
+    console.log('         -шесть элементов <section>  +2');
+    console.log('         -только один заголовок <h1> +2')
+    console.log('         -пять заголовков <h2> +2');
+    console.log('         -один элемент <nav> +2');
+    console.log('         -два списка ul > li > a навигация, ссылки соцсети +2');
+    console.log('         -десять кнопок <button> +2');
+    console.log('         -два инпута: <input type="email"> и <input type="tel"> +2');
+    console.log('         -один элемент <textarea> +2');
+    console.log('         -три атрибута placeholder +2');
+    console.log('48/48  Вёрстка соответствует макету +48');
+    console.log('         -блок <header> +6');
+    console.log('         -секция hero +6');
+    console.log('         -секция skills +6');
+    console.log('         -секция portfolio +6');
+    console.log('         -секция video +6');
+    console.log('         -секция price +6');
+    console.log('         -секция contacts +6');
+    console.log('         -блок <footer> +6');
+    console.log('12/12 Требования к css + 12');
+    console.log('         -используются флексы или гриды +2');
+    console.log('         -при уменьш. масштаба страницы браузера вёрстка по центру +2');
+    console.log('         -фоновый цвет тянется на всю ширину страницы +2');
+    console.log('         -иконки добавлены в формате .svg +2');
+    console.log('         -изображения добавлены в формате .jpg +2');
+    console.log('         -есть favicon +2');
+    console.log('20/20 Интерактивность, реализуемая через css +20');
+    console.log('         -плавная прокрутка по якорям +5');
+    console.log('         -ссылки в футере на гитхаб автора проекта и курс https://rs.school/js-stage0/ +5');
+    console.log('         -интерактивность при наведении и клике, руководствуясь общим стилем макета +5');
+    console.log('         -плавное изменение вида элемента при наведении и клике не влияющее на соседние элементы +5');
+    return console.log('Итого: 110 баллов (Завышеная самооценка. Кто-то должен урезать, у меня рука не поднимается).');
+}
+selfscore();
